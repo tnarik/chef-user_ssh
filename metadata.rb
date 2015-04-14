a2dcd6116661d741d10155c2a05a3db66524f403
tnarik <tnarik@gmail.com>
2015-04-14 11:24:09 +0100

    Moved to single quotes and added source and issues URLs, for the Chef Supermarket.

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,12 @@
-name             "user_ssh"
-maintainer       "Tnarik Innael"
-maintainer_email "tnarik@lecafeautomatique.co.uk"
-license          "apache2"
-description      "A Chef recipe to manage user accounts and SSH keys from databags"
+name             'user_ssh'
+maintainer       'Tnarik Innael'
+maintainer_email 'tnarik@lecafeautomatique.co.uk'
+license          'apache2'
+description      'A Chef recipe to manage user accounts and SSH keys from databags'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+source_url       'https://github.com/tnarik/chef-user_ssh'
+issues_url       'https://github.com/tnarik/chef-user_ssh/issues'
+version          '0.1.0'
 
 %w{user}.each do |cookbook|
   depends cookbook
@@ -14,4 +16,4 @@ end
   supports os
 end
 
-recipe "user_ssh::data_bag", "Processes a list of users with data drawn from a data bag."
+recipe 'user_ssh::data_bag', 'Processes a list of users with data drawn from a data bag.'
