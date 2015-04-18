d9d9d6ed6b2e7edf4904327ee5c4eb3be66a86ac
tnarik <tnarik@lecafeautomatique.co.uk>
2015-04-18 18:51:37 +0100

    Added override to attributes for the 'solaris2' platform to workaround 'fnichol/chef-user/issues/92' (addressing tnarik/chef-user_ssh/issues/1) without requiring specific settings in other cookbooks or nodes. It's magic

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+# Until fnichol/chef-user#92 is fixed, let's override these settings here.
+case platform
+when 'solaris2'
+  default['user']['home_root']      = "/export/home"
+  default['user']['default_shell']  = "/bin/bash"
+end
